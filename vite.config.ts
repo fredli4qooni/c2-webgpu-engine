67db1951917acc25981dec8a0709b0b386707bc1
fredli4qooni <fredlifourqoni999@gmail.com>
2025-12-08 00:26:33 +0700

    chore: prepare for github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,18 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
+const REPO_NAME = '/c2-webgpu-engine/';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  base: REPO_NAME,
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    target: 'esnext',
   },
 });
